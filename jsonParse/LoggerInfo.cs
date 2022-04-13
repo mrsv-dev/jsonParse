6c8d524ea4c7017202004ce5a23cb0162b9b97fc
Yaroslav Kozak <yaroslav.kozak@techfabric.co>
2022-04-13 17:55:23 +0300

    Logger accepts multiple params

--- jsonParse/LoggerInfo.cs
+++ jsonParse/LoggerInfo.cs
@@ -1,29 +1,34 @@
 ﻿using Newtonsoft.Json;
 using Serilog;
 using System;
+using System.Collections.Generic;
 
 namespace DeserializeExtra
 {
     public class LoggerInfo
     {
-        private static Params myCollection;
+        private static List<Params> myCollection = new List<Params>();
         public static readonly ILogger _logger = new LoggerConfiguration().WriteTo.RollingFile(pathFormat: "Log_request.json")
                                                               .WriteTo.Console()
                                                                .WriteTo.Debug()
                                                              .CreateLogger();
 
-        public static void MyLogger(Root root, Period period)
+        public static void MyLogger(Root root)
         {
-            myCollection = new Params()
+            foreach (var period in root.Periods)
             {
-                requestDate = DateTime.Now.ToString(),
-                countPartner = root.Periods.Count,
-                partnerId = period.Id,
-                isValidRules = period.IsValidPayment,
-                PaymentMethod = period.PaymentMethod
-            };
-            string serializedAnswer = JsonConvert.SerializeObject(myCollection);
-            _logger.Information(serializedAnswer);
+                var param = new Params()
+                {
+                    requestDate = DateTime.Now.ToString(),
+                    countPartner = root.Periods.Count,
+                    partnerId = period.Id,
+                    isValidRules = period.IsValidPayment,
+                    PaymentMethod = period.PaymentMethod
+                };
+                string serializedAnswer = JsonConvert.SerializeObject(param);
+                _logger.Information(serializedAnswer);
+                myCollection.Add(param);
+            }
         }
     }
 }
